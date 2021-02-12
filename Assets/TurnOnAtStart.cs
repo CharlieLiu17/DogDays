14684ecc08cfeac25e13534708b0755923acf810
CharlieLiu <charlieliu2222@gmail.com>
2021-02-12 10:52:14 -0500

    Fade In at beginning

---
+++ Assets/TurnOnAtStart.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOnAtStart : MonoBehaviour
+{
+    public GameObject fadeIn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fadeIn.SetActive(true);
+    }
+}
