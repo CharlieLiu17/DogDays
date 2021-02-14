4e3a3ade928fb1b20cfacae583106356f58db159
CharlieLiu <charlieliu2222@gmail.com>
2021-02-13 22:41:31 -0500

    Start Menu

---
+++ Assets/_Scripts/IdleTransition.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleTransition : MonoBehaviour
+{
+    public Animator anim;
+    public float timeLimitConst;
+    private float time = 0;
+    private int count = 0;
+    private float timeLimit;
+
+    private void Start()
+    {
+        timeLimit = timeLimitConst + Random.Range(-3f, 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (count == 1)
+        {
+            anim.SetBool("IdleTransition", false);
+            count = 0;
+        }
+        if (time >= timeLimit)
+        {
+            anim.SetBool("IdleTransition", true);
+            Debug.Log(time);
+            time = 0;
+            count++;
+            timeLimit = timeLimitConst + Random.Range(-3f, 3f);
+        }
+        
+    }
+}
