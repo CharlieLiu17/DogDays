a3fbe017dbaf393ff2e2946240a01477c9ae903c
CharlieLiu <charlieliu2222@gmail.com>
2021-02-25 18:45:50 -0500

    Thing for morris
    Moreris thing

---
+++ Assets/Human_Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Human_Movement : MonoBehaviour
+{
+    public Transform[] locations;
+    public Transform self;
+    public float idleTime;
+
+    private bool moving;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        self = locations[Random.Range(0, locations.Length)];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
