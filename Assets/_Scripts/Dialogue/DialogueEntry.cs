87a00f47191495f9f1d591d75114988992c7d7bf
tbittel6 <tbittel6@gatech.edu>
2021-03-15 20:15:45 -0400

    Add Dialogue System (No XML Yet)

---
+++ Assets/_Scripts/Dialogue/DialogueEntry.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+
+public class DialogueEntry
+{
+    public string displayText;
+    public List<DialogueOption> options; // Each option is assigned to a button in the dialogue UI
+
+    public DialogueHandler dialogueHandler; // The handler of the currently active dialogue
+
+    // Public constructor using XML
+    public DialogueEntry(XmlNode node)
+    {
+
+    }
+
+    public DialogueEntry(DialogueHandler dialogueHandler, string displayText, List<DialogueOption> options)
+    {
+        this.displayText = displayText;
+        this.options = options;
+        if (options == null)
+        {
+            this.options = new List<DialogueOption>();
+
+            // If a DialogueEntry contains no options, give it an option that closes the dialogue UI
+            List<DialogueEvent> events = new List<DialogueEvent>();
+            events.Add(new DialogueEvent(DialogueEventType.EndDialogue));
+            this.options.Add(new DialogueOption(dialogueHandler, "Ok", events));
+        }
+    }
+
+    public override string ToString()
+    {
+        string stringToReturn = displayText + ": [";
+        foreach (DialogueOption option in options) {
+            stringToReturn += option.displayText + ", ";
+        }
+        stringToReturn.Remove(stringToReturn.Length - 1);
+        stringToReturn += "]";
+        return stringToReturn;
+    }
+}
---
+++ Assets/_Scripts/Dialogue/DialogueHandler.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+
+// Attached to a dialogue source like an NPC and given a DialogueEntry.
+public class DialogueHandler : MonoBehaviour
+{
+    private DialogueEntry _currentDialogue;
+
+    [SerializeField]
+    private string dialogueName;
+
+    public DialogueEntry CurrentDialogue
+    {
+        get { return _currentDialogue; }
+        set
+        {
+            _currentDialogue = value;
+            if (_currentDialogue == null)
+            {
+                UIManager.Instance.DialoguePanelShown = false;
+            } else
+            {
+                DisplayCurrentDialogue();
+            }
+        }
+    }
+
+    void Start()
+    {
+        List<DialogueOption> options = new List<DialogueOption>();
+
+        DialogueEntry next = new DialogueEntry(this, "It worked! (That's a miracle!)", null);
+
+        options.Add(new DialogueOption(this, "Test Option 1", null, next, null, true));
+
+        CurrentDialogue = new DialogueEntry(this, "This is test dialogue.", options);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (_currentDialogue.options != null)
+            {
+                foreach (DialogueOption option in _currentDialogue.options)
+                {
+                    if (option.selectOnEnterPressed)
+                    {
+                        option.OnSelect();
+                    }
+                }
+            }
+        }
+    }
+
+    public void DisplayCurrentDialogue()
+    {
+        UIManager.Instance.DisplayDialogue(_currentDialogue);
+    }
+}
