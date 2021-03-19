cf44bb7d1813e2c1027c5f55cf6b7cdb623706dc
CharlieLiu <charlieliu2222@gmail.com>
2021-03-19 18:18:16 -0400

    Foosteps

--- Assets/_Scripts/Footsteps.cs
+++ Assets/_Scripts/Footsteps.cs
@@ -17,17 +17,18 @@ public class Footsteps : MonoBehaviour
     public void Step()
     {
         RaycastHit hit;
-
         audio.pitch = 1 + Random.Range(-0.1f, 0.1f);
         //checks for ground underneath, 0.3 is to account for transform's low position
         if (Physics.Raycast(transform.position + new Vector3(0f, 0.3f, 0f), Vector3.down, out hit))
         {
+            Debug.Log("Woah");
             if (hit.transform.tag == "Wood") //the players .gameObject is there because i'm not sure if you have it set to a transform, if it's a GameObject then you can be rid of it :)
             {
-                audio.PlayOneShot(walkingWood, 0.1f);
+
+                audio.PlayOneShot(walkingWood, 0.05f);
             } else if (hit.transform.tag == "Grass")
             {
-                audio.PlayOneShot(walkingGrass, 0.1f);
+                audio.PlayOneShot(walkingGrass, 0.05f);
             }
         }
     }
